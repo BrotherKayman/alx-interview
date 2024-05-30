32286d8029f076a683236f197bd0f278a533dd73
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-05-30 23:03:58 +0200

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+def pascal_triangle(n):
+    """Generate Pascal's triangle up to the nth row.
+
+    Args:
+        n (int): Number of rows in triangle.
+
+    Returns:
+        list: Pascal's triangle as a list of lists.
+
+    Raises:
+        ValueError: If n <= 0.
+
+    """
+    x = [[1]]
+
+    if n <= 0:
+        return []
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(x[i - 1][j - 1] + x[i - 1][j])
+        row.append(1)
+        x.append(row)
+
+    return x
+
+# Example usage:
